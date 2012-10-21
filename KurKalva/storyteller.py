38e623a6d704ad9255a371c3c24582ed780f4364
Shreya K <satishkalva@Satishs-MacBook-Pro.local>
2012-10-21 16:04:17 -0400

    changes

--- KurKalva/storyteller.py
+++ KurKalva/storyteller.py
@@ -1,1 +1,10 @@
+#!usr/bin/python
+
 #Shreya Kalva & David Kurkovskiy ML7 pd7
+
+from pymongo import Connection
+
+    Connection = Connection('mongo.stuycs.org')
+    database = Connection.admin
+
+
