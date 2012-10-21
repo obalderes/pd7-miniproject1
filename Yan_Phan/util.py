ccb3c70792ff9f2f527e13aa78ca70d746edafa2
Brian Yan <sorakeybade@gmail.com>
2012-10-20 22:43:48 -0400

    More stuff

--- Yan_Phan/util.py
+++ Yan_Phan/util.py
@@ -8,6 +8,14 @@ collection = db.collection1
 
 def newStory(title):
     if collection.find({'title':title}).count() == 0:
-        collection.insert({'title':title,'story':''})
+        collection.insert({'title':title,'story':[]})
+
+def returnStory(title):
+    story = collection.find_one({'title':title})
+    return story
 
 def newLine(title,line):
+    storyTitle = collection.find_one({'title':title})
+    paragraph = storyTitle['story']
+    paragraph.append(line)
+    collection.update({'title':title},{'$set' {'story':paragraph}})
