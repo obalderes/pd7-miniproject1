c68af38ff228d6628121f5f281120dd948b6bc58
Shreya K <satishkalva@Satishs-MacBook-Pro.local>
2012-10-21 19:10:47 -0400

    unit tests for the rest of the methods!

--- KurKalva/storytest.py
+++ KurKalva/storytest.py
@@ -15,8 +15,33 @@ class TestSequenceFunctions(unittest.TestCase):
       storyteller.addStory("test")
       storyteller.addLine("test", "testline")
       lines = storyteller.returnStory("test")['lines']
-      hold = lines[0]
-      self.assertEqual(hold, "testline")
+      self.assertEqual(lines[0], "testline")
+
+   def test_removeStory(self):
+      storyteller.addStory("test")
+      storyteller.removeStory("test")
+      self.assertEqual(storyteller.returnStory("test"), None)
+
+   def test_returnStory(self):
+      storyteller.addStory("test")
+      storyteller.addStory("hello")
+      self.assertEqual(storyteller.returnStory("test")['title'], "test")
+
+   def test_returnStories(self):
+      storyteller.wipeDatabase()
+      storyteller.addStory("test")
+      storyteller.addStory("hello")
+      storyteller.addStory("numba3")
+      numTitles = storyteller.returnStories()
+      self.assertEqual(len(numTitles), 3)
+
+   def test_wipeDatabase(self):
+      storyteller.addStory("test")
+      storyteller.addStory("hello")
+      storyteller.addStory("numba3")
+      storyteller.wipeDatabase()
+      numTitles = storyteller.returnStories()
+      self.assertEqual(len(numTitles), 0)
 
 if __name__ == '__main__':
    unittest.main()
