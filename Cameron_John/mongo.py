b8844b5bd9afe87010a760ecba7de0edcbbc9836
Cameron Abma <Chabma@gmail.com>
2012-10-21 14:22:19 -0700

    Added Connect
    One step at a time...

---
+++ Cameron_John/mongo.py
@@ -0,0 +1,11 @@
+from pymongo import Connection
+
+def connect():
+	Connection = Connection ('mongo.stuycs.org')
+	db = Connection.admin
+	res=db.authenticate('ml7','ml7')
+	db = Connection['z-pd7-Cameron_John']
+	return db
+	
+
+	
