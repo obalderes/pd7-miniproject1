4beffb15f4db20bcf986c5fd0c1a04e4988cdefa
Brian Yan <sorakeybade@gmail.com>
2012-10-20 22:22:22 -0400

    Connection check

--- Yan_Phan/util.py
+++ Yan_Phan/util.py
@@ -1,3 +1,5 @@
 from pymongo import Connection
-
-connection = Connection('mongo.stuycs.org')
+Connection=Connection('mongo.stuycs.org')
+db = Connection.admin
+res=db.authenticate('ml7','ml7')
+db = Connection['z-pd7']
