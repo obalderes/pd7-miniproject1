0d6617731516e378aa2f5fb83203d8cb20d7ac37
David Kurkovskiy <brainmonkey95@gmail.com>
2012-10-21 17:04:46 -0400

    remove method

--- KurKalva/storyteller.py
+++ KurKalva/storyteller.py
@@ -17,12 +17,19 @@ def addStory(x):
     d = {'title': str(x), 'lines': []}
     kurkalva.insert(d)
 
-def addLine(title, newLine):
-    tmp = kurkalva.find_one({'title': title})
-    tmp.lines.append(str(newLine))
+def addLine(x, newLine):
+    res = kurkalva.find_one({'title': x})
+    oldList = res['lines']
+    newList = oldList.append(newLine)
+    kurkalva.update({'title': x}, {'title' : x, 'lines' : newList})
+
+def removeStory(x):
+    kurkalva.remove({'title' : x})
 
 addStory("This is a new story")
 #print db.collection_names()
 #print kurkalva.find_one()
-addLine("This is a new story", "and it sounds a little something like this")
-kurkalva.find_one({'title': "This is a new story"})
+#addLine("This is a new story", "and it sounds a little something like this")
+print kurkalva.find_one({'title': "This is a new story"})
+removeStory("This is a new story")
+print kurkalva.find_one({'title': "This is a new story"})
