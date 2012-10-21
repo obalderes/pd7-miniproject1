4834086eaa20dea7927c865214a14139d16a8f4f
Peter Jasko <peterjasko@new-host-3.home>
2012-10-21 14:44:24 -0400

    Added create,get methods along with a deleteall method

--- Peter_Bernie/mongo.py
+++ Peter_Bernie/mongo.py
@@ -1,6 +1,36 @@
-import pymongo
 from pymongo import Connection
 
-Connection=Connection('mongo.stuycs.org')
-db = Connection.admin
-res=db.authenticate('ml7','ml7')
+def connect_init:
+    Connection=Connection('mongo.stuycs.org')
+    db = Connection.admin
+    res=db.authenticate('ml7','ml7')
+    db = Connection['z-pd7']
+    collection = db.stories
+    return collection
+
+
+
+def newStory(title,story):
+     x = {'title':title,'story':[]}
+     stories = connect_init()
+     if(stories.find({'title': title}).count() = 0):
+         stories.insert(x)
+         
+
+def deleteStory(title):
+    stories = connect_init()
+    if(stories.find({'title': title}).count() != 0):
+        stories.remove(stories.find_one({'title':title}))
+
+def addComment(title,comment):
+    stories = connect_init()
+    if(stories.find({'title': title}).count() != 0):
+        x = stories.find_one({'title':title}
+        x['comments'].append(comment)
+        stories.remove({'title':title})
+        stories.insert(x)
+
+
+def getStories():
+                                 
+                      
