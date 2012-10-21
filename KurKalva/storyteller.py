26b4187c3425f9e4d5fb3abf67579d3b70b263da
David Kurkovskiy <brainmonkey95@gmail.com>
2012-10-21 16:04:30 -0400

    copied Z's stuff

--- KurKalva/storyteller.py
+++ KurKalva/storyteller.py
@@ -1,1 +1,12 @@
+#!usr/bin/python
+
 #Shreya Kalva & David Kurkovskiy ML7 pd7
+
+from pymongo import Connection
+
+Connection=Connection('mongo.stuycs.org')
+
+db = Connection.admin
+res = db.authenticate('ml7', 'ml7')
+
+db = Connection['z-pd7']
