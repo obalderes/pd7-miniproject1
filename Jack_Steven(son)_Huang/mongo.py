4d69d3f48a10a99f62b77b5dfa0f17163656ea0c
kajabla <jskestrel@gmail.com>
2012-10-21 15:03:59 -0300

    Update Jack_Steven(son)_Huang/mongo.py
    fixed "authenticate"

--- Jack_Steven(son)_Huang/mongo.py
+++ Jack_Steven(son)_Huang/mongo.py
@@ -3,5 +3,5 @@ from pymongo import Connection
 Connection=Connection('mongo.stuycs.org')
 
 db=Connection.admin
-res=db.authenticte('ml7','ml7')
+res=db.authenticate('ml7','ml7')
 db=Connection['z-pd7-JackSteven]
