e1ac449c5db2cc04a26c72dc095b84fc99fef086
David Kurkovskiy <brainmonkey95@gmail.com>
2012-10-21 18:13:22 -0400

    trying to figure out unit testing

--- KurKalva/storytest.py
+++ KurKalva/storytest.py
@@ -3,14 +3,13 @@ import storyteller
 
 class TestSequenceFunctions(unittest.TestCase):
 
-    def setUp(self):
-        storyteller.setup()
+   def setUp(self):
+      self.newtitle = 'This is the story that never ends'
 
-    def test_addStory(self):
-        newtitle = 'This is the story that never ends'
-        storyteller.addStory(newtitle)
-        self.assertEqual(storyteller.returnStory()['title'], newtitle)
+   def test_addStory(self):
+      storyteller.addStory(self.newtitle)
+      self.assertEqual(storyteller.returnStory(self.newTitle)['title'], self.newtitle)
 
 
 if __name__ == '__main__':
-    unittest.main()
+   unittest.main()
