478c0b1563feef693a50bd1203b5b2ce4587084b
Brian Yan <sorakeybade@gmail.com>
2012-10-20 22:34:43 -0400

    some more code

--- Yan_Phan/util.py
+++ Yan_Phan/util.py
@@ -3,3 +3,11 @@ Connection=Connection('mongo.stuycs.org')
 db = Connection.admin
 res=db.authenticate('ml7','ml7')
 db = Connection['z-pd7']
+
+collection = db.collection1
+
+def newStory(title):
+    if collection.find({'title':title}).count() == 1:
+        collection.insert({'title':title,'story':''})
+
+def newLine(title,line):
