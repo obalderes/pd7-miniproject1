747d4ebbc8d7925103671886ff7b513c2e33571e
Shreya K <satishkalva@Satishs-MacBook-Pro.local>
2012-10-21 18:43:31 -0400

    edit storytest

--- KurKalva/storytest.py
+++ KurKalva/storytest.py
@@ -11,6 +11,11 @@ class TestSequenceFunctions(unittest.TestCase):
       storyteller.addStory("test")
       self.assertEqual(storyteller.returnStory("test")['title'],"test")
 
+   def test_addLine(self):
+      storyteller.addStory("test")
+      storyteller.addLine("test", "testline")
+      lines = storyteller.returnStory("test")
+      self.assertequal(lines[0], "testline")
 
 if __name__ == '__main__':
    unittest.main()
